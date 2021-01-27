53eff153c1d7eb115e2d2db19fca036d2189d34c
sunmi92 <vbo3129@naver.com>
2021-01-27 15:08:29 +0900

    update

--- user/source/js/detail.js
+++ user/source/js/detail.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+
+    // vr play btn click event
+    $('.playBtn i').click(function(){
+        $(this).removeClass('xi-play-circle-o').addClass('xi-spinner-1 xi-spin');
+        setTimeout(function(){
+            $('iframe').css({'z-index':0})
+        },3000)
+    })
     
     // 스크롤 메뉴 클릭 이벤트
     var menu = $('.scrollMenu li');
@@ -61,52 +69,78 @@ $(document).ready(function() {
     });
 
     // 예약 이벤트
-    $('#start-date').datepicker({
-        dateFormat: 'yy-mm-dd',
+
+    // 시작일 먼저 선택 - > 종료일 선택 제한
+    var startDate = $('#start-date');
+    var startTime = $('#start-date-time');
+    var endDate = $('#end-date');
+    var endTime = $('#end-date-time');
+    var setStartDate = $('#setStartDate');
+    var setStartTime = $('#setStartTime');
+    var setEndDate = $('#setEndDate');
+    var setEndTime = $('#setEndTime');
+    startDate.datepicker({
+        dateFormat: 'yy.mm.dd',
         minDate: 'today',
         onClose: function (selectedDate ) {
+            setStartDate.html(selectedDate);
+            setStartDate.css({'display':'inline-block'});
             var max = $(this).datepicker('getDate');
             if(max == null) {
-                $("#end-date").datepicker('option', 'minDate', 'today' );
-                
-            } else {
-                $("#end-date").datepicker('option', 'minDate', selectedDate );
+                endDate.datepicker('option', 'minDate', 'today' );
+            } else if(getFormatDate($(this).datepicker('getDate')) == getFormatDate(new Date())) {
+                startTime.timepicker('option','minTime',new Date())
+            }else {
+                endDate.datepicker('option', 'minDate', selectedDate );
                 max.setDate(max.getDate() + 1);
-                $('#end-date').datepicker('option', 'maxDate', max);
+                endDate.datepicker('option', 'maxDate', max);
             }
         }
     });
 
-    $('#start-date-time').timepicker({
+    startTime.timepicker({
         interval: 60,
         timeFormat: 'HH:mm',
-        minTime: new Date(),
-    }).on('changeTime', function(){
-        var startTime = $(this).val();
-        $('#end-date-time').timepicker('option','minTIme',startTime +2)
-    })    
-    $('#end-date').datepicker({
-        dateFormat: 'yy-mm-dd',
+        change: function(time) {
+            setStartTime.html($(this).val());
+            setStartTime.css({'display':'inline-block'});
+        }
+    });
+
+    // 종료일 먼저 선택 -> 시작일 선택 제한
+    endDate.datepicker({
+        dateFormat: 'yy.mm.dd',
         minDate: 'today',
         onClose: function (selectedDate) {
             var today = new Date();
             var min = $(this).datepicker('getDate');
+            setEndDate.html(selectedDate);
+            setEndDate.css({'display':'inline-block'});
+
             if(min == null) {
-                $("#start-date").datepicker('option','minDate','today');
+                startDate.datepicker('option','minDate','today');
             } else if(getFormatDate(min) == getFormatDate(today)) {
-                $("#start-date").datepicker('option','minDate', selectedDate);
-                $("#start-date").datepicker('option','maxDate', selectedDate );
-            }else {
+                startDate.datepicker('option','minDate', selectedDate);
+                startDate.datepicker('option','maxDate', selectedDate );
+                endTime.timepicker('option','minTime',startTime.val());
+            } else if(getFormatDate(min) == getFormatDate(startDate.datepicker('getDate'))) {
+                endTime.timepicker('option','minTime',startTime.val());
+            } else {
                 min.setDate(min.getDate() - 1);
-                $("#start-date").datepicker('option','minDate', min);
-                $("#start-date").datepicker('option','maxDate', selectedDate );
+                startDate.datepicker('option','minDate', min);
+                startDate.datepicker('option','maxDate', selectedDate );
             }
         }
     });
-    $('#end-date-time').timepicker({
+
+    
+    endTime.timepicker({
         interval: 60,
         timeFormat: 'HH:mm',
-        
+        change: function(time) {
+            setEndTime.html($(this).val());
+            setEndTime.css({'display':'inline-block'});
+        }
     });
 
     $('#openInfo').click(function () {
@@ -114,6 +148,7 @@ $(document).ready(function() {
         $('.userInfo').css({'display':'block'});
         $('button[form=userReserv]').css({'display':'block'});
     });
+
     
 });
 
