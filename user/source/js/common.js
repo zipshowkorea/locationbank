04412c606170243a045b280f3bca28e49840ae8e
sunmi92 <vbo3129@naver.com>
2021-01-29 13:49:38 +0900

    Update common.js

--- user/source/js/common.js
+++ user/source/js/common.js
@@ -20,4 +20,15 @@ $(document).ready(function(){
             $('.category ul').append('<li style="width:calc(20% * ' + num + ' - 0.1rem); height:4.3rem; position:absolute; bottom:0; right:0"></li>');
         }
     }
+
+    if($(window).width() > 758) {
+        $('.popular_list').parent().removeClass('swiper-container');
+        $('.popular_list').parent().removeClass('swiper-wrapper');
+        $('.popular_list').addClass('grid');
+        $('.popular_list li').removeClass('swiper-slide');
+        $('.search_list').parent().removeClass('swiper-container');
+        $('.search_list').parent().removeClass('swiper-wrapper');
+        $('.search_list').addClass('grid');
+        $('.search_list li').removeClass('swiper-slide');
+    }
 });
